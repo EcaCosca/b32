2b76de89ca4377125cda67075d0ba028f6a08517
EcaCosca <enriquecoscarelli@gmail.com>
2022-10-05 17:54:06 +0200

    Operator equals

---
+++ s3-javascript-lecture/operators.js
@@ -0,0 +1,11 @@
+// https://platform.wbscodingschool.com/courses/full-stack-web-app/11279/
+
+// Operator
+// How do I get my code into the else?
+
+if (1 === true) {
+    console.log("OK");
+  } else {
+    console.log("NOK");
+  }
+  
