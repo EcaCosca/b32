fd670e7b6e0606b4e3cdd2dc021ae74787867485
EcaCosca <enriquecoscarelli@gmail.com>
2022-10-06 12:05:40 +0200

    minmax solution

---
+++ s3-javascript-lecture/minmax.js
@@ -0,0 +1,10 @@
+// https://platform.wbscodingschool.com/courses/full-stack-web-app/11318/
+
+// Min/Max
+
+// We have the values: 9,11,55,22
+// How do we find the minimum and maximum value? Display your result in the console.
+
+const arr = [9,11,55, 22];
+console.log("This is the min", Math.min(...arr));
+console.log("This is the max", Math.max(...arr));
