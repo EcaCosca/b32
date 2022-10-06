c30d9248fcb58f57a5384d785df37b57802bbdca
EcaCosca <enriquecoscarelli@gmail.com>
2022-10-06 10:41:04 +0200

    solition to for

---
+++ s3-javascript-lecture/for.js
@@ -0,0 +1,12 @@
+// https://platform.wbscodingschool.com/courses/full-stack-web-app/11284/
+
+// For
+
+// On this array: ['one', 'two', 'three']
+// Make a for loop and, at each turn, display the value in the console.
+
+const myArray = ["one", "two", "three"];
+
+for (let i=0;i<myArray.length;i++){
+  console.log(myArray[i])
+}
