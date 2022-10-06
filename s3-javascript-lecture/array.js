5aa6c1efb122ae3f9db1392c9dd2aa81c3500d65
EcaCosca <enriquecoscarelli@gmail.com>
2022-10-06 09:54:23 +0200

    array solved

---
+++ s3-javascript-lecture/array.js
@@ -0,0 +1,14 @@
+// https://platform.wbscodingschool.com/courses/full-stack-web-app/11286/
+
+// Array
+
+// We have the values 4,3,5,8; put these values in an array and display in the console:
+// all the values
+// the first value
+// the length of the array
+
+const arr = [4, 3, 5, 8];
+
+console.log("all the values are "+ arr)
+console.log("the first value is "+ arr[0])
+console.log("the length of the array is " + arr.length)
