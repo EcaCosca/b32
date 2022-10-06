21d80bd2edfc045141d3a99f2646321827205247
EcaCosca <enriquecoscarelli@gmail.com>
2022-10-06 10:16:20 +0200

    Solving sort

---
+++ s3-javascript-lecture/arraySort.js
@@ -0,0 +1,9 @@
+// https://platform.wbscodingschool.com/courses/full-stack-web-app/11285/
+
+// Array Sort
+
+// We have the values 4,3,5,8; put these values in a array, sort this array and console.log() this array.
+
+const arr = [4, 3, 5, 8]
+
+console.log(arr.sort())
