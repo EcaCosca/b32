52fff9e61e0dd082d83e8d1c9f5129e6d7afb98a
EcaCosca <enriquecoscarelli@gmail.com>
2022-10-07 10:01:10 +0200

    Descriving fizz buzz

--- s3-javascript-lecture/fizzbuzz.js
+++ s3-javascript-lecture/fizzbuzz.js
@@ -1,1 +1,14 @@
 // https://platform.wbscodingschool.com/courses/full-stack-web-app/12105/
+
+// Fizz Buzz
+
+// FizzBuzz is a programming task, used in software developer job interviews.
+
+// If a number is a multiple of 3 the function returns fizz
+// If a number is a multiple of 5 the function returns buzz
+// If a number is a multiple of 3 AND 5 the function returns fizzbuzz
+
+// As a sequence it would be:
+// 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, Fizz Buzz, 31, 32, Fizz, 34, Buzz, Fizz
+
+// Create a function in index.js which takes a number and return fizz or buzz or fizzbuzz or the number.
