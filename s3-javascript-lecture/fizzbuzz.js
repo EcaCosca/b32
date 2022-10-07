8fb7d000049b08de8453a393e10322dbfa93a678
EcaCosca <enriquecoscarelli@gmail.com>
2022-10-07 10:01:26 +0200

    Solving fizz buzz

--- s3-javascript-lecture/fizzbuzz.js
+++ s3-javascript-lecture/fizzbuzz.js
@@ -12,3 +12,21 @@
 // 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, Fizz Buzz, 31, 32, Fizz, 34, Buzz, Fizz
 
 // Create a function in index.js which takes a number and return fizz or buzz or fizzbuzz or the number.
+
+function fizzbuzz(number) {
+    if (number % 3 === 0 && number % 5 === 0) {
+      console.log("Fizz Buzz")
+    } else if (number % 3 === 0) {
+      console.log("Fizz")
+    } else if (number % 5 === 0) {
+      console.log("Buzz")
+    } else {
+      console.log(number)
+    }
+  }
+  
+  for(let i=0;i<36;i++){
+    fizzbuzz(i)
+  }
+  
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder
