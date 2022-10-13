8ddaf619d7030327bfa422de62ede3b854dc023e
EcaCosca <enriquecoscarelli@gmail.com>
2022-10-13 13:25:54 +0200

    OOP literal done

---
+++ s3-oop/literal.js
@@ -0,0 +1,19 @@
+// https://platform.wbscodingschool.com/courses/full-stack-web-app/11282/
+
+// Object
+// Create a JSON with 2 students:
+// - first name : Han / last name : Solo
+// - first name : Chew / last name : Bacca
+
+const starwars = [
+    {
+      firstName: "Han",
+      lastName: "Solo"
+    },
+    {
+      firstName: "Chew",
+      lastName: "Bacca"
+    }
+  ]
+  
+  console.log(JSON.stringify(starwars))
