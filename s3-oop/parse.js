55a7892ed022db7460b76e462770a6058cd26c5a
EcaCosca <enriquecoscarelli@gmail.com>
2022-10-13 13:27:22 +0200

    Done with parse

--- s3-oop/parse.js
+++ s3-oop/parse.js
@@ -1,3 +1,9 @@
+https://platform.wbscodingschool.com/courses/full-stack-web-app/11281/
+
+// Object
+// Without changing the variable in index.js
+// Transform the text in object and display the value of "first_name".
+
 const text = '{"name" : "Dupont", "first_name" : "Jean"}';
 
 console.log(JSON.parse(text).first_name)
