a651895047a4f2f50bbaea504a96593e50921c8c
EcaCosca <enriquecoscarelli@gmail.com>
2022-10-19 13:36:03 +0200

    Nizami done

--- lollocalStorage/app.js
+++ lollocalStorage/app.js
@@ -1,1 +1,22 @@
-alert('Working')
+const h1 = document.querySelector('h1')
+const form = document.querySelector('.form')
+const input = document.querySelector('.input')
+const submit = document.querySelector('.submit')
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+
+    // CHANGE VALUE OF H1 
+    h1.innerHTML = input.value
+
+    // SAVE IN LOCAL STORAGE
+    localStorage.setItem("input", input.value) 
+}
+
+const handleLoad = () => {
+    localStorage.getItem('input') ? h1.innerHTML = localStorage.getItem('input') : h1.innerHTML = 'localStorage'
+}
+
+handleLoad()
+
+form.addEventListener('submit', handleSubmit)
